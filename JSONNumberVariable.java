782648b7d70e5c61146f67162bdf73b1e03daf98
unknown <austin.lemler@cuw.edu>
2015-11-24 10:20:37 -0600

    export JSON

---
+++ JSONNumberVariable.java
@@ -0,0 +1,27 @@
+public class JSONNumberVariable extends JSONVariable
+{
+	private int value;
+	
+	public JSONNumberVariable(String name, int value) 
+	{
+		super(name);
+		this.value = value;
+			}
+
+	@Override
+	void display() 
+	{
+		System.out.println("Number : " + this.name + " -> " + this.value);
+	}
+
+	@Override
+	String exportToJSON() 
+	{
+		String answer = ":";
+		answer += this.value;
+		answer += ",";
+		answer += this.name + " -> " + answer;
+		return answer;
+	}
+
+}
---
+++ JSONObjectVariable.java
@@ -0,0 +1,23 @@
+public class JSONObjectVariable extends JSONVariable
+{
+	private JSONObject value;
+	
+	public JSONObjectVariable(String name, JSONObject value)
+	{
+		super(name);
+		this.value = value;
+	}
+
+	void display() 
+	{
+		System.out.println(this.name +" -> ");
+		this.value.display();
+	}
+
+	@Override
+	String exportToJSON() 
+	{
+	 System.out.println(this.name +" ->");	
+	 return this.value.exportToJSON();
+	}
+}
